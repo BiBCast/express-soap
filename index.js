d38111df5605db2e05647dc14e54308c16fec2d8
BiBCast <pietrocast03@gmail.com>
2023-12-07 17:21:55 +0100

    feature: TODO test api

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
-const soap = require("soap");
+"use strict";
+import axios from "axios";
 
-// Create the SOAP client
 const url =
   "http://srvx3v12h.formula.it:8124/soap-wsdl/syracuse/collaboration/syracuse/CAdxWebServiceXmlCC?wsdl";
 
 var credentials = { Authorization: "Basic YWRtaW46YWRtaW4=" };
-soap.createClient(url, function (err, client) {
-  client.addSoapHeader(credentials);
-  /*   if (err) {
-    //console.error("Error creating SOAP client:", err);
-    return;
-  } */
-  //console.log(client);
 
-  // Make a SOAP request
-  const args = { Authorization: "Basic YWRtaW46YWRtaW4=" };
-  client.getDescription(args, function (err, result) {
-    if (err) {
-      console.error("Error making SOAP request:", err);
-      return;
+let webApiUrl = url;
+let tokenStr = "YWRtaW46YWRtaW4=";
+
+async function getUsers() {
+  try {
+    // 👇️ const data: GetUsersResponse
+    const { data, status } = await axios.get(webApiUrl, {
+      headers: {
+        Accept: "application/json",
+        credentials,
+      },
+    });
+
+    console.log(JSON.stringify(data, null, 4));
+
+    // 👇️ "response status is: 200"
+    console.log("response status is: ", status);
+
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log("error message: ", error.message);
+      return error.message;
+    } else {
+      console.log("unexpected error: ", error);
+      return "An unexpected error occurred";
     }
+  }
+}
 
-    // Handle the SOAP response
-    console.log("result:", result);
-  });
-});
+getUsers();
